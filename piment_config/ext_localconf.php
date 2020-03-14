fe6892ab13fbfe4963e09d6c6395b92b29a0d59b
Dmitry Vasilev <d.vasilev@connecta.ag>
2020-03-14 20:38:16 +0300

    Init extension piment_rouge

---
+++ piment_config/ext_localconf.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+
+// Add BackendLayouts BackendLayouts for the BackendLayout DataProvider
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:piment_config/Configuration/PageTS/Mod/WebLayout/BackendLayouts.txt">');
+
+// TSconfig
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:piment_config/Configuration/PageTS/TSConfig.txt">');
+
+// RTE
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:piment_config/Configuration/PageTS/RTE.txt">');
+
+/***************
+ * Add default RTE configuration for piment_config
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['pimentrouge'] = 'EXT:piment_config/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['productionExceptionHandler'] = '';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['debugExceptionHandler'] = '';
